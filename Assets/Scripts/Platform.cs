ff537c00dc3f2750f2ed457c19b7f304712e52ef
Charlie <charlieryancurry@gmail.com>
2025-01-26 15:26:29 -0400

    Update Platform.cs

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
+    public MeshCollider meshCollider;
+
+    public PlayerMovement playerMovement;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerMovement = FindObjectOfType<PlayerMovement>();
+        meshCollider.isTrigger = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerMovement.transform.position.y > transform.position.y)
+        {
+            meshCollider.isTrigger = false;
+        }
     }
 }
